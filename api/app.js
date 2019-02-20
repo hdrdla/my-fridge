3f906ee7d88a454bfbba53d7561e86b0e50a87d9
Aleum <aleum@Katrinas-MacBook-Pro.local>
2019-02-20 10:34:12 +0100

    bodyparser added

--- api/app.js
+++ api/app.js
@@ -5,13 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
-
+var bodyParser = require('body-parser')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
